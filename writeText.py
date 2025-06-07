5766cff5a10c4e13a1d0668ab5d31b86a7824bbc
DharmBharodiya <dharmbharodiya@gmail.com>
2025-06-07 15:12:05 +0530

    extra

--- writeText.py
+++ writeText.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((600, 600, 3), dtype="uint8")
+# blank = np.zeros((600, 600, 3), dtype="uint8")
 
-cv.putText(blank, 'Dharm', (225, 225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
+# cv.putText(blank, 'Dharm', (225, 225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
 
-cv.imshow("Text", blank)
+# cv.imshow("Text", blank)
+
+img = cv.imread("images/fourth.jpg")
+cv.putText(img, "Dharm", (255,255), cv.FONT_ITALIC, 1.0, (255, 255,255), 1)
+cv.imshow("Txt on image", img)
 
 cv.waitKey(0)
